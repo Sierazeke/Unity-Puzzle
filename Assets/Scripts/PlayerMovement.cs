029b4bf13e4fb224bf5af6d9bda7bb62b5e9b61a
kristaps <kristapsandis@inbox.lv>
2026-02-11 22:06:53 +0200

    Added a very basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 25f;
+
+    public Transform cameraTransform;
+
+    float xRotation = 0;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
